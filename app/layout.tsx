bebd0e657553881b3b4b5744075f7798aee8828e
Devang-sharma609 <devang122indmp@gmail.com>
2025-03-17 19:07:36 +0530

    vercel web analytics enabled

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "@/app/globals.css"
 import { Montserrat } from 'next/font/google'
 import type { Metadata } from "next"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Analytics } from '@vercel/analytics/next';
 
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -31,6 +32,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   )
